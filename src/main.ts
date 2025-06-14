ae84e922f36881c353e075f84b09f7329cfa1fde
Bakhtiar <bakhtiarfahim360@gmail.com>
2025-06-14 09:43:09 +0600

    middleware added

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import * as express from 'express';
 import { GlobalErrorHandlerFilter } from './error/globalErrorHandler.filter';
 import { ValidationPipe } from '@nestjs/common';
 // import { UserSeeder } from './seed/admin.seed';
@@ -10,6 +11,15 @@ async function bootstrap() {
     rawBody: true,
     bodyParser: true,
   });
+  app.use(
+    express.json({
+      verify: (req: any, res, buf) => {
+        if (req.headers['stripe-signature']) {
+          req.rawBody = buf;
+        }
+      },
+    }),
+  );
   app.enableCors();
   app.use(cookieParser());
   app.enableCors();
